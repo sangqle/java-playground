4429bf58f195bef190251adf98758dc66613f56d
sanglq3 <sanglq3@vng.com.vn>
2023-04-06 23:20:56 +0700

    completed sample signing data:

--- synchronous-dumy/src/main/java/com/sangqle/sample/crypto/SignatureExample.java
+++ synchronous-dumy/src/main/java/com/sangqle/sample/crypto/SignatureExample.java
@@ -8,9 +8,11 @@ import java.security.*;
 import java.security.spec.*;
 import javax.crypto.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SignatureExample {
 
+    @Deprecated
     private static void writeKeysToFile(PrivateKey privateKey, PublicKey publicKey, String privateKeyFile, String publicKeyFile) throws IOException {
         // Write the private key to file
         try (FileOutputStream fos = new FileOutputStream(privateKeyFile)) {
@@ -81,19 +83,75 @@ public class SignatureExample {
         verifier.update(data);
         return verifier.verify(signature);
     }
+
+    public static boolean verifyAndPrint(byte[] data, byte[] signature, PublicKey publicKey) throws Exception {
+        Signature sig = Signature.getInstance("SHA256withRSA");
+        sig.initVerify(publicKey);
+        sig.update(data);
+        boolean verified = sig.verify(signature);
+        if (verified) {
+            String message = new String(data, StandardCharsets.UTF_8);
+            System.err.println("Signature verified, message: " + message);
+        } else {
+            System.err.println("Signature not verified");
+        }
+        return verified;
+    }
+
+    public static String buildHash(Map<String, Object> data) throws NoSuchAlgorithmException {
+        // Sort the keys in the map
+        String[] keys = data.keySet().toArray(new String[0]);
+        Arrays.sort(keys);
+
+        // Concatenate the values in sorted order
+        String content = Arrays.stream(keys)
+            .map(k -> {
+                Object value = data.get(k);
+                if (value instanceof Map) {
+                    try {
+                        return buildHash((Map<String, Object>) value);
+                    } catch (NoSuchAlgorithmException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    return String.valueOf(value);
+                }
+            })
+            .collect(Collectors.joining());
+
+        // Calculate the SHA-256 hash
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(content.getBytes(StandardCharsets.UTF_8));
+
+        // Convert the hash to a hex string
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        return hexString.toString();
+    }
+
     public static void main(String[] args) throws Exception {
         // Generate a public-private key pair
 //        writeKeysToFile();
         PrivateKey privateKey = readPrivateKeyFromFile("private_key.txt");
         PublicKey publicKey = readPublicKeyFromFile("public_key.txt");
 
-        String message = "Hello, world!";
+        String message = buildHash(new HashMap<String, Object>() {{
+            put("a", 1);
+            put("b", 2);
+            put("c", 3);
+        }});
+
+        System.err.println("Message " + message);
         byte[] signature = sign(message.getBytes(StandardCharsets.UTF_8), privateKey);
 
+        String signatureString = Base64.getEncoder().encodeToString(signature);
+        System.out.println("Signature: " + signatureString);
+
         // Verify the signature using the public key
-        boolean verified = verify(message.getBytes(StandardCharsets.UTF_8), signature, publicKey);
+        boolean verified = verifyAndPrint(message.getBytes(StandardCharsets.UTF_8), signature, publicKey);
         System.err.println("Signature verified: " + verified);
-
-
     }
 }
